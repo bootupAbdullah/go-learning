f63c2a900258d85c79568bfcd0afcd9bbf2c3c67
Abdullah Durrani <134801570+bootupAbdullah@users.noreply.github.com>
2025-12-20 14:23:13 -0500

    tour of go, practice go

--- go-tour/practice.go
+++ go-tour/practice.go
@@ -4,13 +4,6 @@ import (
 	"fmt"
 )
 
-const Pi = 3.14
-
 func main() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go Rules?", Truth)
+	
 }
